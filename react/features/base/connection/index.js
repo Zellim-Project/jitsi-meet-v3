ec971095166729d3075d23c0f3a55cc28e55df2e
christiancuri <c.christiancuri@gmail.com>
2022-03-28 03:53:48 -0300

    fix: missing enable end for all

--- react/features/base/connection/index.js
+++ react/features/base/connection/index.js
@@ -1,6 +1,7 @@
 // @flow
 
-export * from './actions';
+// export * from './actions';
+export * from '../../../custom/connection/actions';
 export * from './actionTypes';
 export * from './constants';
 export * from './functions';
